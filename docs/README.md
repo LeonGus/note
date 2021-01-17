ed656e10988b13e3af54c78551f246f07c3d0c5c
leon.liang <leongus.liang@yunlsp.com>
2021-01-17 14:34:43 +0800

    feat:发布页面

---
+++ docs/README.md
@@ -0,0 +1,9 @@
+<!--
+ * @Author: Leon
+ * @Date: 2021-01-17 13:42:37
+ * @version: 0.0.1
+ * @Descripttion: 这是注释
+ * @LastEditors: Leon
+ * @LastEditTime: 2021-01-17 13:51:39
+-->
+# Hello World
---
+++ docs/config.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: Leon
+ * @Date: 2021-01-17 13:45:19
+ * @version: 0.0.1
+ * @Descripttion: 这是注释
+ * @LastEditors: Leon
+ * @LastEditTime: 2021-01-17 13:53:19
+ */
+module.exports = {
+    title: `leon's Note`,
+    description: '笔记',
+    base: '/note/',
+    head: [
+        ['link', { rel: 'icon', href: '/logo.jpg' }]
+      ]
+}
